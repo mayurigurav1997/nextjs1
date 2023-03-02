21eaa457606b75ea62ba564b2231c4a130116da6
Mayuri Gurav <mayurigurav1997@gmail.com>
2023-03-03 01:02:33 +0530

    navbar added

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,16 @@ export default function Home() {
         <meta name="keywords" content="next ,hunting coder, huntingcoder blog" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Script src="/sc.js" strategy="lazyOnload"></Script>
+      {/* <Script src="/sc.js" strategy="lazyOnload"></Script>
+       */}
+      <nav className={styles.mainnav}>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Blog</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
       <main className={styles.main}>
         <h1 className={styles.title}>Hunting Coder</h1>
 
@@ -24,63 +33,24 @@ export default function Home() {
           A hunting coder by hunting coder
         </p>
 
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
+        <h2>Popular Blogs</h2>
+        <div className="blogs">
+          <div className="blogItem">
+            <h3>How to learn Javascript in 2023?</h3>
+            <p>Javascript is the language used to design logic for web.</p>
+          </div>
+          <div className="blogItem">
+            <h3>How to learn Javascript in 2023?</h3>
+            <p>Javascript is the language used to design logic for web.</p>
+          </div>
+          <div className="blogItem">
+            <h3>How to learn Javascript in 2023?</h3>
+            <p>Javascript is the language used to design logic for web.</p>
+          </div>
+          <div className="blogItem">
+            <h3>How to learn Javascript in 2023?</h3>
+            <p>Javascript is the language used to design logic for web.</p>
+          </div>
         </div>
       </main>
     </>
