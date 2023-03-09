d4dc6c11262e9b38e16649845fcdfaa5185abadf
Mayuri Gurav <mayurigurav1997@gmail.com>
2023-03-09 10:58:56 +0530

    Datablogs added

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  // console.log(req)
-  // console.log(res)
-  res.status(200).json({ name: 'John Doe' })
+    // console.log(req)
+    // console.log(res)
+    res.status(200).json({ name: 'This is my Home page' })
 }
