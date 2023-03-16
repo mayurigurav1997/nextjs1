7ea6a192269da5aee0c824cb10749c277df6f5ef
Mayuri Gurav <mayurigurav1997@gmail.com>
2023-03-16 22:22:32 +0530

    postcontact created

--- pages/api/postcontact.js
+++ pages/api/postcontact.js
@@ -1,8 +1,13 @@
-export default function handler(req, res) {
+import * as fs from 'fs';
+
+export default async function handler(req, res) {
     if (req.method === 'POST') {
         // console.log(req.body)
+        let data = await fs.promises.readdir('contactdata')
+        console.log(data)
+        fs.promises.writeFile(`contactdata/${data.length + 1}.json`, JSON.stringify(req.body))
+
         res.status(200).json(req)
-        // Process a POST request
     } else {
         res.status(200).json(["allBlogs"])
         // name,email,desc,phone
