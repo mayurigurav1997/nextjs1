b6647ddb76c1bf0c1a729eacf3c00f7dcf6ddea6
Mayuri Gurav <mayurigurav1997@gmail.com>
2023-03-17 22:53:10 +0530

    form layout added in contact page

--- pages/contact.js
+++ pages/contact.js
@@ -1,9 +1,42 @@
-import React from 'react'
-import styles from '@/styles/Home.module.css'
+import React, { useState } from 'react'
+import styles from '@/styles/Contact.module.css'
 
+const handleSubmit = (e) => {
+    e.preventDefault()
+}
+const handleChange = (e) => {
+
+}
 const contact = () => {
+    const [name, setname] = useState("")
+    const [email, setemail] = useState("")
+    const [phone, setphone] = useState("")
+    const [desc, setdesc] = useState("")
     return (
-        <div className={styles.main}>This is contact page.</div>
+        <div className={styles.container}>
+            <h1>Contact Us</h1>
+            <form onSubmit={handleSubmit}>
+                <div className={styles.mb3}>
+                    <label for="name" className={styles.formlabel}>Enter your Name</label>
+                    <input type="text" className="form-control" id="name" aria-describedby="emailHelp" name="name" value={name} onChange={handleChange} />
+                </div>
+                <div className={styles.mb3}>
+                    <label for="email" className={styles.formlabel}>Email address</label>
+                    <input type="email" className="form-control" id="email" name="email" aria-describedby="emailHelp" value={email} onChange={handleChange} />
+                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div className={styles.mb3}>
+                    <label for="phone" className={styles.formlabel}>Phone</label>
+                    <input type="phone" className="form-control" name="phone" id="phone" value={phone} onChange={handleChange} />
+                </div>
+                <div className={styles.mb3}>
+                    <label for="desc">Elbroarate your concern</label>
+                    <textarea className="form-control" placeholder="Write your concern here" id="desc" value={desc} onChange={handleChange}></textarea>
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+
+        </div>
     )
 }
 
